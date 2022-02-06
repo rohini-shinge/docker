47049f3dbac2bd1b4f4c4d621fc37ca3e5739170
Avani Shinge <shingerohini712@gmail.com>
2022-02-06 17:35:32 +0530

    Test package changes

--- api-gateway/src/test/java/com/learning/microservices/apigateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/learning/microservices/apigateway/ApiGatewayApplicationTests.java
@@ -1,4 +1,4 @@
-package com.in28minutes.microservices.apigateway;
+package com.learning.microservices.apigateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
