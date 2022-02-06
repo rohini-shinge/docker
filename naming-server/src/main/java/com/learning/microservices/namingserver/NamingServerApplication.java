78dcd60af7a8d1bf019626b2f6aa0c28a96eddb2
Avani Shinge <shingerohini712@gmail.com>
2022-02-06 17:42:23 +0530

    Package restructuring

--- naming-server/src/main/java/com/learning/microservices/namingserver/NamingServerApplication.java
+++ naming-server/src/main/java/com/learning/microservices/namingserver/NamingServerApplication.java
@@ -1,4 +1,4 @@
-package com.in28minutes.microservices.namingserver;
+package com.learning.microservices.namingserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- naming-server/src/test/java/com/learning/microservices/namingserver/NamingServerApplicationTests.java
+++ naming-server/src/test/java/com/learning/microservices/namingserver/NamingServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.in28minutes.microservices.namingserver;
+package com.learning.microservices.namingserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
